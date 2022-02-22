2f2ce6a31b71c3f7443c37ad163688679678d186
not-tnb <79249093+Not-TNB@users.noreply.github.com>
2022-02-22 18:24:47 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ window.onbeforeunload = function() {
   window.scrollTo(0, 0);
 }
 
+// initialize AOS (Animate On Scroll)
+AOS.init({ 
+  duration: 600,
+  offset: 0
+});
+
 var book_cover = document.getElementById('coverBtn')
 var body = document.getElementsByTagName("body")[0]
 var rest = document.getElementById('rest')
