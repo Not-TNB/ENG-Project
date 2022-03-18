d8750505a7a03d331a9bb835d06c88af0e9bbe08
not-tnb <79249093+Not-TNB@users.noreply.github.com>
2022-03-18 10:34:48 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,62 @@
-window.onbeforeunload = function() { window.scrollTo(0, 0); }
-
-// initialize AOS (Animate On Scroll)
-AOS.init({ 
+AOS.init({
   duration: 800,
-  offset: 100,
-  disable: 'mobile'
+  offset: 0
 });
 
-var book_cover = document.getElementById('coverBtn')
-var body = document.getElementsByTagName('body')[0]
-var container = document.getElementById('container')
+window.onbeforeunload = function() {
+  history.scrollRestoration = 'manual'
+  window.scrollTo(0, 0)
+}
 
-book_cover.addEventListener('change', function() {
-  if (this.checked) {
-    container.style.opacity = '100%'
-    body.style.overflowY = 'scroll'
-  } else {
-    container.style.opacity = '0%'
-    body.style.overflowY = 'hidden'
-    window.scrollTo(0, 0);
-  }
-})
+async function getTime(id, location) {
+  document.getElementById(id).innerHTML = new Date().toLocaleString('en-GB', {
+    timeZone: location,
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+  }) 
+}
+
+function updateLocations() {
+  getTime('kyivTime', 'Africa/Cairo') // Kyiv and Cairo have the same timezone :D
+  getTime('londonTime', 'Europe/London')
+  getTime('parisTime', 'Europe/Paris')
+  getTime('nycTime', 'America/New_York')
+  getTime('tokyoTime', 'Asia/Tokyo')
+  getTime('moscowTime', 'Europe/Moscow')
+  getTime('dubaiTime', 'Asia/Dubai')
+  getTime('beijingTime', 'Asia/Shanghai') // China only has one timezone so they put shanghai
+  getTime('sydneyTime', 'Australia/Sydney')
+  getTime('rjTime', 'Brazil/East') // Who the fuck created this naming system
+  getTime('cairoTime', 'Africa/Cairo')
+}
+
+var today = new Date()
+var h = (today.getHours() < 10) ? '0' + today.getHours() : today.getHours()
+var m = (today.getMinutes() < 10) ? '0' + today.getMinutes() : today.getMinutes()
+var s = (today.getSeconds() < 10) ? '0' + today.getSeconds() : today.getSeconds()
+document.getElementById('localTime').innerHTML = h+':'+m+':'+s
+
+document.getElementById('details').innerHTML = today.toLocaleTimeString('en-us',{timeZoneName:'short'}).split(' ')[2]
+
+updateLocations()
+
+var delayH = -3600*(h+6)-60*m
+var delayM = -60*(30+m)
+var delayS = -30.25-s
+document.documentElement.style.setProperty('--delayH', delayH + 's')
+document.documentElement.style.setProperty('--delayM', delayM + 's')
+document.documentElement.style.setProperty('--delayS', delayS + 's')
+
+setInterval(function() {
+  var today = new Date()
+  var h = (today.getHours() < 10) ? '0' + today.getHours() : today.getHours()
+  var m = (today.getMinutes() < 10) ? '0' + today.getMinutes() : today.getMinutes()
+  var s = (today.getSeconds() < 10) ? '0' + today.getSeconds() : today.getSeconds()
+  document.getElementById('localTime').innerHTML = h+':'+m+':'+s
+
+  updateLocations()
+}, 1)
